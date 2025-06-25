b7e9983d5e4a1800d79a5be48087c211503906b1
Sonashree-05 <123352656+Sonashree-05@users.noreply.github.com>
2025-06-25 16:51:50 +0530

    Update email_slicer_program.py

--- email_slicer_program.py
+++ email_slicer_program.py
@@ -3,3 +3,15 @@ username = email[:email.index('@')]        #This start from example and ends bef
 domain = email[email.index('@')+1:]        #can also write as email.index('g'), here this starts with index [7+1:] till end. 
 sliced = (f"Your username is '{username}' and domain is '{domain}'")    #f refers to formatter
 print(sliced)
+
+
+
+'''An email slicer is the best Python project for beginners. The project retrieves the username and domain of an email and prints a message with this information to send to the host.  
+
+Steps to create an Email Slicer in Python-
+
+Get the email from the user with the input method. 
+With the strip() method we will get the characters we want, that is, only the email address.
+Then separate the username and domain name.
+Save the extracted username and domain name in a single statement.
+Using print() the message with the username and domain name will be sent to the host.'''
